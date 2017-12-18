ff49dee569bd6788f44f002b329f530be30ab8a9
Vi Lo <mars86582lo@gmail.com>
2017-12-18 22:10:32 +0400

    spirit page with galleries

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var pictures = document.querySelectorAll(".gallery_item");
+var previousImageArrow = document.querySelector("#previous_pic");
+var nextImageArrow = document.querySelector("#next_pic");
+var count = 1;
+
+function previousImage (handler) {
+	if (count > 1) {
+		pictures[count-1].parentNode.classList.remove('current');
+		pictures[count-2].parentNode.classList.add('current');	
+		count -= 1;
+		console.log(count);
+	} else if (count === 1) {
+		pictures[count-1].parentNode.classList.remove('current');
+		pictures[2].parentNode.classList.add('current');
+		count = 3;
+		console.log(count);	
+	}
+}
+
+function nextImage (handler) {
+	if (count < 3) {
+		pictures[count-1].parentNode.classList.remove('current');
+		pictures[count].parentNode.classList.add('current');	
+		count += 1;
+	} else if (count === 3) {
+		pictures[count-1].parentNode.classList.remove('current');
+		pictures[0].parentNode.classList.add('current');
+		count = 1;	
+	}
+}
+
+previousImageArrow.addEventListener("click", previousImage, false);
+nextImageArrow.addEventListener("click", nextImage, false);
